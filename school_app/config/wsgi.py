87a83f9e031968ad909b6a73fbed47b58afeef16
Landry-kayoyo <22nb396@esisalama.org>
2026-08-02 09:36:49 +0200

    Fix: ajout réel du sys.path dans wsgi.py

--- school_app/config/wsgi.py
+++ school_app/config/wsgi.py
@@ -1,5 +1,12 @@
 import os
+import sys
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(ROOT_DIR))
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 application = get_wsgi_application()
